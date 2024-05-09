885b3fa1a7a92a0c0bd603ce734b1428ecae3da2
MegaSilver1 <vitass2003ss@gmail.com>
2024-05-09 22:36:50 +0300

    first and second lab

---
+++ UtmVitalik/Controllers/BrowseController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace UtmVitalik.Controllers
+{
+    public class BrowseController : Controller
+    {
+        public ActionResult Browse()
+        {
+            return View();
+        }
+    }
+}
---
+++ UtmVitalik/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UtmVitalik.MvcApplication" Language="C#" %>
