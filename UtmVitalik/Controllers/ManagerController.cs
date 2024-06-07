9d60e7cb6140b6b65cd6aed9e4dfc1587750a914
MegaSilver1 <vitass2003ss@gmail.com>
2024-06-08 02:13:13 +0300

    small progress

---
+++ UtmVitalik/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace UtmVitalik.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public ActionResult Manager()
+        {
+            return View();
+        }
+    }
+}
